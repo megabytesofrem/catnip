c798fbd7c471f1413c6d0804c2447624268e4fc9
megabytesofrem <dhillonwilk@gmail.com>
2026-02-19 06:15:01 +0000

    Maybe and Either typeclasses

--- Catnip.Core/Data/Either.cs
+++ Catnip.Core/Data/Either.cs
@@ -1,6 +1,6 @@
 namespace Catnip.Core.Data;
 
-public sealed class EitherW { }
+public sealed class EitherW<L> { }
 
 /// <summary>
 /// The Either type represents a value that can be one of two types, Left or Right.
@@ -9,13 +9,102 @@ public sealed class EitherW { }
 /// </summary>
 /// <typeparam name="L"></typeparam>
 /// <typeparam name="R"></typeparam>
-public abstract record Either<L, R> : HKT<EitherW, (L, R)>
+public abstract record Either<L, R> : HKT<EitherW<L>, R>
 {
     public sealed record Left(L Value) : Either<L, R>;
     public sealed record Right(R Value) : Either<L, R>;
 
     // Projection function to extract the Either<L, R> from the HKT.
-    public static Either<L, R> Proj(HKT<EitherW, (L, R)> h) => (Either<L, R>)h;
+    public static Either<L, R> Proj(HKT<EitherW<L>, R> h) => (Either<L, R>)h;
+}
+
+public sealed class EitherK<L>
+    : Eq<EitherW<L>>,
+      Functor<EitherW<L>>,
+      Applicative<EitherW<L>>,
+      Monad<EitherW<L>>,
+      Semigroup<EitherW<L>>
+{
+
+    // Dummy instance needed to satisfy the type class constraints
+    public static readonly EitherK<L> Instance = new EitherK<L>();
+
+    #region Applicative/Monad
+
+    // ap : Either a (b -> c) -> Either a b -> Either a c
+    public HKT<EitherW<L>, B> Ap<A, B>(HKT<EitherW<L>, Func<A, B>> mf, HKT<EitherW<L>, A> ma)
+    {
+        var ef = Either<L, Func<A, B>>.Proj(mf);
+        var ea = Either<L, A>.Proj(ma);
+
+        return (ef, ea) switch
+        {
+            (Either<L, Func<A, B>>.Left(var l), _) => Either.Left<L, B>(l),
+            (_, Either<L, A>.Left(var l)) => Either.Left<L, B>(l),
+            (Either<L, Func<A, B>>.Right(var f), Either<L, A>.Right(var a)) => Either.Right<L, B>(f(a)),
+            _ => throw new InvalidOperationException()
+        };
+    }
+
+    // bind : Either a b -> (b -> Either a c) -> Either a c
+    public HKT<EitherW<L>, B> Bind<A, B>(HKT<EitherW<L>, A> ma, Func<A, HKT<EitherW<L>, B>> f)
+    {
+        var ea = Either<L, A>.Proj(ma);
+        return ea switch
+        {
+            Either<L, A>.Left(var l) => Either.Left<L, B>(l),
+            Either<L, A>.Right(var a) => f(a),
+            _ => throw new InvalidOperationException()
+        };
+    }
+
+    // fmap : Either a b -> (b -> c) -> Either a c
+    public HKT<EitherW<L>, B> FMap<A, B>(Func<A, B> f, HKT<EitherW<L>, A> ma)
+    {
+        var e = Either<L, A>.Proj(ma);
+        return e switch
+        {
+            Either<L, A>.Left(var l) => Either.Left<L, B>(l),
+            Either<L, A>.Right(var r) => Either.Right<L, B>(f(r)),
+            _ => throw new InvalidOperationException()
+        };
+    }
+
+    // pure : a -> Either a b
+    public HKT<EitherW<L>, A> Pure<A>(A a) => Either.Right<L, A>(a);
+    #endregion
+
+    #region Eq & Semigroup
+
+    // eq : Eq a => Either a b -> Either a b -> bool
+    public bool Eq<A>(HKT<EitherW<L>, A> x, HKT<EitherW<L>, A> y)
+    {
+        var ex = Either<L, A>.Proj(x);
+        var ey = Either<L, A>.Proj(y);
+        return (ex, ey) switch
+        {
+            (Either<L, A>.Left(var l1), Either<L, A>.Left(var l2)) => EqualityComparer<L>.Default.Equals(l1, l2),
+            (Either<L, A>.Right(var r1), Either<L, A>.Right(var r2)) => EqualityComparer<A>.Default.Equals(r1, r2),
+            _ => false
+        };
+    }
+
+    // sconcat : Either a b -> Either a b -> Either a b
+    public HKT<EitherW<L>, A> SConcat<A>(HKT<EitherW<L>, A> x, HKT<EitherW<L>, A> y)
+    {
+        var ex = Either<L, A>.Proj(x);
+        var ey = Either<L, A>.Proj(y);
+        return (ex, ey) switch
+        {
+            (Either<L, A>.Left(var l1), Either<L, A>.Left(var l2)) => Either.Left<L, A>(l1), // or l2, they are equal
+            (Either<L, A>.Left(var l), _) => Either.Left<L, A>(l),
+            (_, Either<L, A>.Left(var l)) => Either.Left<L, A>(l),
+            (Either<L, A>.Right(var r1), Either<L, A>.Right(var r2)) => Either.Right<L, A>(r1), // or r2, they are equal
+            _ => throw new InvalidOperationException()
+        };
+    }
+
+    #endregion
 }
 
 /// <summary>
--- Catnip.Core/Data/Maybe.cs
+++ Catnip.Core/Data/Maybe.cs
@@ -19,8 +19,88 @@ public abstract record Maybe<A> : HKT<MaybeW, A>
 /// The Maybe type represents an optional value, either Just or Nothing.
 /// </summary>
 /// <typeparam name="A"></typeparam>
-public static class Maybe
+public sealed class Maybe
+    : Eq<MaybeW>,
+      Functor<MaybeW>,
+      Applicative<MaybeW>,
+      Monad<MaybeW>,
+      Semigroup<MaybeW>
 {
     public static Maybe<A> Just<A>(A value) => new Maybe<A>.Just(value);
     public static Maybe<A> Nothing<A>() => new Maybe<A>.Nothing();
+
+    #region Applicative/Monad
+
+    // ap : Maybe (a -> b) -> Maybe a -> Maybe b
+    public HKT<MaybeW, B> Ap<A, B>(HKT<MaybeW, Func<A, B>> mf, HKT<MaybeW, A> ma)
+    {
+        var mf_ = Maybe<Func<A, B>>.Proj(mf);
+        var ma_ = Maybe<A>.Proj(ma);
+        return (mf_, ma_) switch
+        {
+            (Maybe<Func<A, B>>.Nothing, _) => Nothing<B>(),
+            (_, Maybe<A>.Nothing) => Nothing<B>(),
+            (Maybe<Func<A, B>>.Just f, Maybe<A>.Just a) => Just(f.Value(a.Value)),
+
+            _ => throw new InvalidOperationException("Invalid Maybe value")
+        };
+    }
+
+    // fmap : Maybe a -> (a -> b) -> Maybe b
+    public HKT<MaybeW, B> FMap<A, B>(Func<A, B> f, HKT<MaybeW, A> ma)
+    {
+        var ma_ = Maybe<A>.Proj(ma);
+        return ma_ switch
+        {
+            Maybe<A>.Nothing => Nothing<B>(),
+            Maybe<A>.Just a => Just(f(a.Value)),
+            _ => throw new InvalidOperationException("Invalid Maybe value")
+        };
+    }
+
+    // bind : Maybe a -> (a -> Maybe b) -> Maybe b
+    public HKT<MaybeW, B> Bind<A, B>(HKT<MaybeW, A> ma, Func<A, HKT<MaybeW, B>> f)
+    {
+        var ma_ = Maybe<A>.Proj(ma);
+        return ma_ switch
+        {
+            Maybe<A>.Nothing => Nothing<B>(),
+            Maybe<A>.Just a => f(a.Value),
+            _ => throw new InvalidOperationException("Invalid Maybe value")
+        };
+    }
+
+    // pure : a -> Maybe a
+    public HKT<MaybeW, A> Pure<A>(A a) => Just(a);
+    #endregion
+
+    #region Eq & Semigroup
+
+    // eq : Eq a => Maybe a -> Maybe a -> bool
+    public bool Eq<A>(HKT<MaybeW, A> x, HKT<MaybeW, A> y)
+    {
+        var mx = Maybe<A>.Proj(x);
+        var my = Maybe<A>.Proj(y);
+        return (mx, my) switch
+        {
+            (Maybe<A>.Nothing, Maybe<A>.Nothing) => true,
+            (Maybe<A>.Just a, Maybe<A>.Just b) => EqualityComparer<A>.Default.Equals(a.Value, b.Value),
+            _ => false
+        };
+    }
+
+    // sconcat : Maybe a -> Maybe a -> Maybe a
+    public HKT<MaybeW, A> SConcat<A>(HKT<MaybeW, A> x, HKT<MaybeW, A> y)
+    {
+        var mx = Maybe<A>.Proj(x);
+        var my = Maybe<A>.Proj(y);
+        return (mx, my) switch
+        {
+            (Maybe<A>.Nothing, _) => y,
+            (_, Maybe<A>.Nothing) => x,
+            (Maybe<A>.Just a, Maybe<A>.Just b) => Just(a.Value),
+            _ => throw new InvalidOperationException("Invalid Maybe value")
+        };
+    }
+    #endregion
 }
